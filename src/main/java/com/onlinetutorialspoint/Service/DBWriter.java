9e4257f4974a61d6aaf67430c5a0692c9b482050
sharm <nitin.rebelee91@gmail.com>
2020-03-22 13:20:18 +0530

    CSV Writer

--- src/main/java/com/onlinetutorialspoint/Service/DBWriter.java
+++ src/main/java/com/onlinetutorialspoint/Service/DBWriter.java
@@ -6,10 +6,15 @@ import org.springframework.batch.item.ItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 @Component
 public class DBWriter implements ItemWriter<Item> {
+
+    File file = new File("C:\\Intellij Code\\BatchProcessing\\src\\main\\resources\\Item1.csv");
 
     @Autowired
     private ItemRepository itemRepository;
@@ -17,6 +22,21 @@ public class DBWriter implements ItemWriter<Item> {
     @Override
     public void write(List<? extends Item> items) throws Exception {
         itemRepository.saveAll(items);
+        FileWriter fileWriter = new FileWriter(file);
+        items.forEach(a -> {
+            try {
+                fileWriter.append(String.valueOf(a.getId()));
+                fileWriter.append(",");
+                fileWriter.append(a.getCategory());
+                fileWriter.append(",");
+                fileWriter.append(a.getName());
+                fileWriter.append("\n");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        fileWriter.flush();
+        fileWriter.close();
         System.out.println(items);
     }
 }
--- src/main/java/com/onlinetutorialspoint/controller/ItemController.java
+++ src/main/java/com/onlinetutorialspoint/controller/ItemController.java
@@ -1,72 +1,48 @@
 package com.onlinetutorialspoint.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.*;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @RestController
 public class ItemController {
+    private static Logger log = LoggerFactory.getLogger(ItemController.class);
     @Autowired
     private Job job;
     @Autowired
     private JobLauncher jobLauncher;
+    private AtomicInteger atomicInteger = new AtomicInteger(0);
 
     @GetMapping(value = "/Load", produces = "application/json")
-    public BatchStatus load() throws JobParametersInvalidException, JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException {
+    public ResponseEntity load() throws JobParametersInvalidException
+            , JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException {
         Map<String, JobParameter> maps = new HashMap<>();
         maps.put("time", new JobParameter(System.currentTimeMillis()));
         JobParameters parameters = new JobParameters(maps);
         JobExecution jobExecution = jobLauncher.run(job, parameters);
         System.out.println("Job Status" + jobExecution.getStatus());
-        return jobExecution.getStatus();
-    }
-  /*  @RequestMapping("/getAllItems")
-    @ResponseBody
-    public ResponseEntity<List<Item>> getAllItems(){
-        List<Item> items =  itemRepo.getAllItems();
-        return new ResponseEntity<List<Item>>(items, HttpStatus.OK);
+        return new ResponseEntity(HttpStatus.OK);
     }
 
-    @GetMapping("/item/{itemId}")
-    @ResponseBody
-    public ResponseEntity<Item> getItem(@PathVariable int itemId){
-        if(itemId <= 0){
-            throw new ItemNotFoundException("Invalid ItemId");
-        }
-        Item item = itemRepo.getItem(itemId);
-        return new ResponseEntity<Item>(item, HttpStatus.OK);
+    @GetMapping(value = "/getData", produces = "application/json")
+    public ResponseEntity service() throws InterruptedException {
+//        Thread.sleep(1000);
+
+        log.info("Distributed Logging {}", atomicInteger.incrementAndGet());
+        return new ResponseEntity(HttpStatus.OK);
     }
 
-    @PostMapping(value = "/addItem",consumes = {"application/json"},produces = {"application/json"})
-    @ResponseBody
-    public ResponseEntity<Item> addItem(@RequestBody Item item,UriComponentsBuilder builder){
-        itemRepo.addItem(item);
-        HttpHeaders headers = new HttpHeaders();
-        headers.setLocation(builder.path("/addItem/{id}").buildAndExpand(item.getId()).toUri());
-        return new ResponseEntity<Item>(headers, HttpStatus.CREATED);
-    }
-
-    @PutMapping("/updateItem")
-    @ResponseBody
-    public ResponseEntity<Item> updateItem(@RequestBody Item item){
-        if(item != null){
-            itemRepo.updateItem(item);
-        }
-        return new ResponseEntity<Item>(item, HttpStatus.OK);
-    }
-
-    @DeleteMapping("/delete/{id}")
-    @ResponseBody
-    public ResponseEntity<Void> deleteItem(@PathVariable int id){
-        itemRepo.deleteItem(id);
-        return new ResponseEntity<Void>(HttpStatus.ACCEPTED);
-    }*/
 }
